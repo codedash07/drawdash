939c2f17455016b3206e7a0e260e5940bb13bf8e
Aryan Agarwal <aryankush025@gmail.com>
2023-12-26 21:07:45 +0530

    Update meta tags and add hydration to Index component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,17 @@
-import { Suspense } from "react";
 import type { MetaFunction } from "@remix-run/node";
 
 import Draw from "../Draw.client";
+import useHydrate from "../hooks/hydrating";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Drawdash" },
+    { name: "description", content: "Welcome to Drawdash!" },
   ];
 };
 
 export default function Index() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Draw />
-    </Suspense>
-  );
+  const isHydrated = useHydrate();
+
+  return isHydrated ? <Draw /> : <div>Loading...</div>;
 }
