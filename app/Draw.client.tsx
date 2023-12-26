8bb608e4aa846250dea57bd8e46d5db7f453e6d3
Aryan Agarwal <aryankush025@gmail.com>
2023-12-26 21:07:39 +0530

    Refactor Draw component

--- app/Draw.client.tsx
+++ app/Draw.client.tsx
@@ -1,10 +1,12 @@
 import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 
-export default function Draw() {
+function Draw() {
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw />
     </div>
   );
 }
+
+export default Draw;
