aaf9dfffb378dd7c5e7e34e10c62618e5b237fe3
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 02:10:53 +0530

    Add new route for Drawdash component

---
+++ app/routes/new.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from "@remix-run/node";
+
+import Draw from "../components/Draw.client";
+import useHydrate from "../hooks/hydrating";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Drawdash" },
+    { name: "description", content: "Welcome to Drawdash!" },
+  ];
+};
+
+export default function Index() {
+  const isHydrated = useHydrate();
+
+  return isHydrated ? <Draw /> : <div>Loading...</div>;
+}
