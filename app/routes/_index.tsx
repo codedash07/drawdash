9c4efcc2e7eae289914dddd664e44e6f25ccc07d
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 12:58:36 +0530

    Add drawing list to index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
+import { Key } from "react";
 import { json, LoaderFunction, type MetaFunction } from "@remix-run/node";
 import { getUser, requireUserId } from "../utils/session.server";
 import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "../utils/db.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,16 +15,21 @@ export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const user = await getUser(request);
 
+  const drawings = await db.drawing.findMany({
+    where: {
+      creatorId: userId,
+    },
+  });
+
   return json({
     userId,
     user,
+    drawings,
   });
 };
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
-
-  console.log("#### data", data);
 
   return (
     <div>
@@ -31,8 +38,8 @@ export default function Index() {
           <span className="text-xl font-bold">{`Hi ${data.user.username}`}</span>
           <form action="/logout" method="post">
             <button
-              type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
             >
               Logout
             </button>
@@ -40,15 +47,28 @@ export default function Index() {
 
           <form action="/new" method="post">
             <button
-              type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
             >
               New Drawing
             </button>
           </form>
+
+          <ol>
+            {data.drawings.map((drawing: { id: Key; name: string }) => (
+              <li key={drawing.id}>
+                <Link
+                  to={`/drawing/${drawing.id}`}
+                  className="text-blue-500 hover:underline"
+                >
+                  {drawing.name}
+                </Link>
+              </li>
+            ))}
+          </ol>
         </div>
       ) : (
-        <Link to="/login" className="text-blue-500 hover:text-blue-700">
+        <Link to="/login" className="text-blue-500 hover:underline">
           Login
         </Link>
       )}
