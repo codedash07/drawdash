8ec0b37eb9230f06f326b9aebf14a0f1a432f3d3
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 12:39:50 +0530

    Add initialDrawingJson constant

---
+++ app/utils/constants.ts
@@ -0,0 +1,247 @@
+export const initialDrawingJson = {
+  store: {
+    "document:document": {
+      gridSize: 10,
+      name: "",
+      meta: {},
+      id: "document:document",
+      typeName: "document",
+    },
+    "page:page": {
+      meta: {},
+      id: "page:page",
+      name: "Page 1",
+      index: "a1",
+      typeName: "page",
+    },
+    "shape:-1GuDxZukTx6YCxcgwTOe": {
+      x: 258.82421875,
+      y: 265.54296875,
+      rotation: 0,
+      isLocked: false,
+      opacity: 1,
+      meta: {},
+      id: "shape:-1GuDxZukTx6YCxcgwTOe",
+      type: "draw",
+      props: {
+        segments: [
+          {
+            type: "free",
+            points: [
+              {
+                x: 0,
+                y: 0,
+                z: 0.5,
+              },
+              {
+                x: -1.04,
+                y: 0,
+                z: 0.5,
+              },
+              {
+                x: -10.75,
+                y: 0,
+                z: 0.5,
+              },
+              {
+                x: -31.96,
+                y: 1.71,
+                z: 0.5,
+              },
+              {
+                x: -53.82,
+                y: 7.69,
+                z: 0.5,
+              },
+              {
+                x: -67.13,
+                y: 14.93,
+                z: 0.5,
+              },
+              {
+                x: -72.83,
+                y: 20.26,
+                z: 0.5,
+              },
+              {
+                x: -74.96,
+                y: 27.73,
+                z: 0.5,
+              },
+              {
+                x: -74.22,
+                y: 39.19,
+                z: 0.5,
+              },
+              {
+                x: -65.07,
+                y: 56.49,
+                z: 0.5,
+              },
+              {
+                x: -40.64,
+                y: 83.03,
+                z: 0.5,
+              },
+              {
+                x: -10.99,
+                y: 107.9,
+                z: 0.5,
+              },
+              {
+                x: 12.99,
+                y: 123.84,
+                z: 0.5,
+              },
+              {
+                x: 35.93,
+                y: 134.29,
+                z: 0.5,
+              },
+              {
+                x: 57.42,
+                y: 139.01,
+                z: 0.5,
+              },
+              {
+                x: 79.15,
+                y: 136.36,
+                z: 0.5,
+              },
+              {
+                x: 101.26,
+                y: 125.29,
+                z: 0.5,
+              },
+              {
+                x: 123.13,
+                y: 107.29,
+                z: 0.5,
+              },
+              {
+                x: 145.4,
+                y: 87.15,
+                z: 0.5,
+              },
+              {
+                x: 163.8,
+                y: 68.44,
+                z: 0.5,
+              },
+              {
+                x: 178.61,
+                y: 50.82,
+                z: 0.5,
+              },
+              {
+                x: 189.79,
+                y: 34.39,
+                z: 0.5,
+              },
+              {
+                x: 197.92,
+                y: 19.08,
+                z: 0.5,
+              },
+              {
+                x: 203.78,
+                y: 4.37,
+                z: 0.5,
+              },
+              {
+                x: 207.16,
+                y: -8.35,
+                z: 0.5,
+              },
+              {
+                x: 208.83,
+                y: -17.9,
+                z: 0.5,
+              },
+              {
+                x: 209.6,
+                y: -24.49,
+                z: 0.5,
+              },
+              {
+                x: 209.65,
+                y: -27.87,
+                z: 0.5,
+              },
+              {
+                x: 209.65,
+                y: -29.18,
+                z: 0.5,
+              },
+            ],
+          },
+        ],
+        color: "black",
+        fill: "none",
+        dash: "draw",
+        size: "m",
+        isComplete: true,
+        isClosed: false,
+        isPen: false,
+      },
+      parentId: "page:page",
+      index: "a1",
+      typeName: "shape",
+    },
+  },
+  schema: {
+    schemaVersion: 1,
+    storeVersion: 4,
+    recordVersions: {
+      asset: {
+        version: 1,
+        subTypeKey: "type",
+        subTypeVersions: {
+          image: 2,
+          video: 2,
+          bookmark: 0,
+        },
+      },
+      camera: {
+        version: 1,
+      },
+      document: {
+        version: 2,
+      },
+      instance: {
+        version: 22,
+      },
+      instance_page_state: {
+        version: 5,
+      },
+      page: {
+        version: 1,
+      },
+      shape: {
+        version: 3,
+        subTypeKey: "type",
+        subTypeVersions: {
+          group: 0,
+          text: 1,
+          bookmark: 1,
+          draw: 1,
+          geo: 7,
+          note: 4,
+          line: 1,
+          frame: 0,
+          arrow: 2,
+          highlight: 0,
+          embed: 4,
+          image: 2,
+          video: 1,
+        },
+      },
+      instance_presence: {
+        version: 5,
+      },
+      pointer: {
+        version: 1,
+      },
+    },
+  },
+};
