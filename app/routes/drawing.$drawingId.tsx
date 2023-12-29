beb539b19f54030b725feba934a33f2043ca1c98
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 13:37:14 +0530

    Refactor drawing route to include loader function and useLoaderData hook

--- app/routes/drawing.$drawingId.tsx
+++ app/routes/drawing.$drawingId.tsx
@@ -1,7 +1,28 @@
-import { useParams } from "@remix-run/react";
+import { LoaderFunction, json } from "@remix-run/node";
+import { useLoaderData, useParams } from "@remix-run/react";
+import { db } from "../utils/db.server";
+import { requireUserId } from "../utils/session.server";
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+
+  const drawings = await db.drawing.findUniqueOrThrow({
+    where: {
+      id: params.drawingId,
+      creatorId: userId,
+    },
+  });
+
+  return json({
+    drawings,
+  });
+};
 
 const Drawing = () => {
   const { drawingId } = useParams();
+  const data = useLoaderData<typeof loader>();
+
+  console.log("#### data", data);
 
   return (
     <div>
