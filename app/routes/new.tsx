ff5d42b9479409157294471c52ee72162233687e
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 15:52:38 +0530

    Remove unused constant from new route

--- app/routes/new.tsx
+++ app/routes/new.tsx
@@ -1,7 +1,6 @@
 import { redirect, ActionFunction } from "@remix-run/node";
 import { db } from "../utils/db.server";
 import { requireUserId } from "../utils/session.server";
-import { initialDrawingJson } from "../utils/constants";
 
 export const action: ActionFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -10,7 +9,6 @@ export const action: ActionFunction = async ({ request }) => {
     data: {
       creatorId: userId,
       name: "New Drawing",
-      content: JSON.stringify(initialDrawingJson),
     },
   });
 
