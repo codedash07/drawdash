8d9ccf2c6eb5e54d4cd9c0604f8c939342e81d62
Aryan Agarwal <aryankush025@gmail.com>
2023-12-29 12:50:00 +0530

    Refactor Drawing component signature

--- app/routes/drawing.$drawingId.tsx
+++ app/routes/drawing.$drawingId.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 interface DrawingProps {
   drawingId: string;
 }
 
-const Drawing: React.FC<DrawingProps> = ({ drawingId }) => {
+const Drawing = ({ drawingId }: DrawingProps) => {
   return (
     <div>
       <h1>Drawing {drawingId}</h1>
